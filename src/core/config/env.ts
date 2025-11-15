25308a80b53cb4d381ae290b004e33fea798d567
Leonardo Rodrigues <leonardo.rodrigues@dopster.io>
2025-11-14 22:43:38 -0300

    feat: add swagger

--- src/core/config/env.ts
+++ src/core/config/env.ts
@@ -1,15 +1,15 @@
-// src/core/config/env.ts
 import * as dotenv from 'dotenv';
 import { z } from 'zod';
 
 dotenv.config();
 
 const envSchema = z.object({
-  DATABASE_URL: z.string().url(),
   PORT: z.coerce.number().default(3000),
+  DATABASE_URL: z.string().url(),
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
 });
 
 export const env = envSchema.parse(process.env);
+console.log('✅ ENV carregado:', env);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { env } from './core/config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Minha API')
+    .setDescription('API usando NestJS, Prisma e Scalar')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(env.PORT ?? 3000);
+  console.log('🚀 HTTP server running on http://localhost:3000');
+  console.log('📄 Docs available at http://localhost:3000/docs');
 }
 bootstrap();
